69771349d0e09a9a76cc197090649a16b5cdbb0b
kaihengann <annkaiheng@gmail.com>
2019-06-08 18:17:46 +0800

    Initial commit for kaaler

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const data = {
+//   result: [
+//     [214, 78, 69],
+//     [247, 242, 163],
+//     [201, 216, 147],
+//     [57, 141, 112],
+//     [62, 80, 64]
+//   ]
+// };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      color: "#e15426"
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ color: "#2b8f9b" });
+  };
+  handleClick2 = () => {
+    this.setState({ color: "#1c1f2a" });
+  };
+
+  componentDidMount = async () => {
+    try {
+      const url = "http://colormind.io/api/";
+      const inputData = {
+        body:
+          '{ "input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+      };
+      const response = await fetch(url, inputData);
+      if (!response.ok) throw new Error("API is broken!"); // Can also use response.status >= 400
+      const data = await response.json();
+      this.setState({ data: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <div id="app" style={{ "--color-page-bg": `${this.state.color}` }}>
+        <button className="generateColor" onClick={this.handleClick}>
+          1
+        </button>
+        <button className="generateColor" onClick={this.handleClick2}>
+          2
+        </button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
+    );
+  }
 }
-
 export default App;
