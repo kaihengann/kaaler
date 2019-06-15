49814e83bd75efa8439e359e6f4afd1301597586
kaihengann <annkaiheng@gmail.com>
2019-06-15 21:06:55 +0800

    Add tests for Card component

--- src/tests/Card.test.js
+++ src/tests/Card.test.js
@@ -4,5 +4,9 @@ import { render } from "@testing-library/react"
 import "@testing-library/react/cleanup-after-each"
 import Card from "../components/Card"
 
-test('should render an icon in the card', () => {
+test('should render card with header and text', () => {
+  const { getByText } = render(<Card header="hi" text="hello"/>)
+  expect(getByText("hi")).toBeInTheDocument();
+  expect(getByText("hello")).toBeInTheDocument();
+  // How to test for icon?
 });
