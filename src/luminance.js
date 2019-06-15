46b2e38cc6b1cc1e7952d0946e78691d5ab1a32d
kaihengann <annkaiheng@gmail.com>
2019-06-15 20:26:08 +0800

    Add invalid input detection for various functions

--- src/luminance.js
+++ src/luminance.js
@@ -1,5 +1,10 @@
 export const partialLuminance = arr => {
+  if (!Array.isArray(arr) || arr.length === 0)
+    throw new Error("Input is not valid");
+
   const newArr = arr.map(value => {
+    if (value < 0 || value > 255) throw new Error("Input is not a valid color");
+
     const colorSRGB = value / 255;
     if (colorSRGB <= 0.03928) {
       return colorSRGB / 12.92;
@@ -11,5 +16,12 @@ export const partialLuminance = arr => {
 };
 
 export const relativeLuminance = arr => {
+  if (!Array.isArray(arr) || arr.length === 0)
+    throw new Error("Input is not valid");
+
+  arr.forEach(value => {
+    if (value >= 1 || value < 0) throw new Error("Input is not valid");
+  });
+
   return 0.2126 * arr[0] + 0.7152 * arr[1] + 0.0722 * arr[2];
 };
--- src/rgbToHex.js
+++ src/rgbToHex.js
@@ -1,4 +1,11 @@
 export const rgbToHex = rgb => {
+  if (!Array.isArray(rgb) || rgb.length === 0)
+    throw new Error("Input is not valid");
+
+  rgb.forEach(value => {
+    if (value < 0 || value > 255) throw new Error("Input is not a valid color");
+  });
+
   return (
     "#" +
     ("0" + parseInt(rgb[0]).toString(16)).slice(-2) +
